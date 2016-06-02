762a2b94a6da40eb0f98c9840b5a5999ca559d3b
Awdesh Sharma <awdesh.sharma1@gmail.com>
2016-06-01 22:44:51 -0700

    rafactoring middleware (#2)

--- web-server/server.js
+++ web-server/server.js
@@ -1,20 +1,7 @@
 var express = require("express");
 var app = express();
-
-// application level middleware, runs for all the application level. like logger which logs in
-// result from all the application pages.
-// routing level middleware, runs just once for the specified route like login page.
-
-var middleware = {
-    requireAuthentication: function(req, res, next){
-        console.log('private route hit!');
-        next();
-    },
-    logger: function(req, res, next){
-        console.log('Request:' + req.method + ' ' + req.originalUrl + ' ' + new Date().toString());
-        next();
-    }
-};
+var middleware = require("./middleware.js");
+var PORT = process.env.PORT || 3000;
 
 app.use(middleware.logger);
 
