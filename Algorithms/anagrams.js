a0f6599c903a43fca4b83b7034b7a39ff604b55e
Awdesh Sharma <arafel5@gmail.com>
2016-06-04 23:36:28 +0000

    improved anagram

--- Algorithms/anagrams.js
+++ Algorithms/anagrams.js
@@ -17,19 +17,29 @@ function areAnagrams(s1, s2){
         index++;
     }
     
+    console.log('length of hash table before is-: ', Object.keys(ht).length);
+    
     var len2 = s2.length;
     var index2 = 0;
     while(index2 < len2){
         if(ht[s2[index2]]){
             ht[s2[index2]] -= 1;
+            if(ht[s2[index2]] === 0){
+                delete ht[[s2[index2]]];
+            }
         } else {
             return false;
         }
         index2++;
     }
     
-    return true;
+    console.log('length of hash table after is-: ', Object.keys(ht).length);
+    
+    if(Object.keys(ht).length == 0)
+        return true;
+    else
+        return false;
 }
 
-var isAnagram = areAnagrams('abcd', 'dacba');
+var isAnagram = areAnagrams('abcd', 'dacb');
 console.log(isAnagram);
